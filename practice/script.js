2cd4b392231429e94d0eee09b4dd49a85f1e774a
Arshree <98222843+Arshree@users.noreply.github.com>
2025-08-04 13:34:36 +0530

    frist code

---
+++ practice/script.js
@@ -0,0 +1,60 @@
+//let age = 15;
+// let mode = "dark";
+// let color;
+
+// if (age > 18){
+//     console.log("you are able to vote");
+// }
+// else{
+//     console.log("you cannot vote");
+// }
+
+// if(mode === "dark"){
+//     color = "black";
+// }
+// else{
+//     color= "white";
+// }
+//
+// console.log(color);
+
+//  let a = 15;
+//  let b = 1;
+
+// if (no%2 == 0){
+//     console.log(no, "Even no");
+// }
+// else {
+//     console.log(no, "Odd no");
+// }
+
+//unary operaters ( increment, decodeURIComponent, post increment, 
+// pre increment, post incremnet, pre increment);
+// console.log("a = ", a, "b= ", b);
+// a++;
+// ++a;
+// a--;
+// --a;
+// console.log("a = ", --a);
+
+// console.log( " a =", a);
+
+//console.log(" a = ", a);
+
+
+// assignmnet operaters
+let a = 12;
+let b = 2;
+let c = "12";
+
+//console.log(" a = ", a++); //4
+// console.log(" a = ", a += 1); //6
+// console.log(" a = ", a -= 1); //5
+// console.log(" a = ", a *= 5); //25
+// console.log(" a = ", a /= 5); //5
+// console.log(" a = ", a %= 5); //1
+// console.log(" a = ", a **= 2); //16
+// console.log(" a=b = ", a != b);
+// console.log(" a=b = ", a ==b );
+console.log(" a are equal or not with database check c: ", a === c);
+console.log(" a are equal or not with database check c: ", a !== c);
